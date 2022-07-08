f53e827fc3447d9db77bee461924ae8703a43afe
Surendra892 <100985781+Surendra892@users.noreply.github.com>
2022-07-08 16:12:10 +0530

    update

--- server/api.js
+++ server/api.js
@@ -16,7 +16,7 @@ app.get("/categories", (req, res)=> {
     mongoclient.connect(connectionstring,(err,clientObj)=>{
         if(!err){
             var database= clientObj.db("demodb");
-            database.collection("tblCategories").find({}).toArray((err,documents)=>{
+            database.collection("tblProducts").find({}).toArray((err,documents)=>{
                 if(!err) {
                     res.send(documents);
                     res.end();
@@ -26,12 +26,12 @@ app.get("/categories", (req, res)=> {
     })
 });
 
-app.get("/category/:id",(req,res)=>{
-    var id = parseInt(req.params.id);
+app.get("/category/:categoryname",(req,res)=>{
+    var CategoryName = req.params.categoryname;
     mongoclient.connect(connectionstring,(err,clientObj)=>{
         if(!err){
             var database= clientObj.db("demodb");
-            database.collection("tblCategories").find({categoryid:id}).toArray((err,documents)=>{
+            database.collection("tblProducts").find({Category:CategoryName}).toArray((err,documents)=>{
                 if(!err) {
                     res.send(documents);
                     res.end();
@@ -64,7 +64,7 @@ app.get("/product/:id",(req,res)=>{
             database.collection("tblProducts").find({Productid:id}).toArray((err,documents)=>{
                 if(!err) {
                     res.send(documents);
-                    res.end();
+                    
                 }
             })
         }
@@ -104,6 +104,36 @@ app.post("/addproduct",(req,res)=>{
             database.collection("tblProducts").insertOne(newproduct,(err,result)=>{
                 if(!err){
                     console.log("new product added");
+                }
+            })
+        }
+    })
+});
+
+app.patch("/updatedata/:id",(req,res)=>{
+    var id = parseInt(req.params.id);
+
+    mongoclient.connect(connectionstring,(err,clientObj)=>{
+        if(!err){
+            var database = clientObj.db("demodb");
+            database.collection("tblProducts").updateOne({Productid:id},{$set:{Name:(req.body.Name),Price:parseFloat(req.body.Price),Stock:(req.body.Stock=="true")?true:false,Category:(req.body.Category),Rating:parseFloat(req.body.Rating)}},(err,result)=>{
+                if(!err){
+                    console.log("Product updated");
+                }
+            })
+        }
+    })
+});
+
+app.delete("/deleteproduct/:id",(req,res)=>{
+    var id = parseInt(req.params.id);
+
+    mongoclient.connect(connectionstring,(err,clientObj)=>{
+        if(!err){
+            var database = clientObj.db("demodb");
+            database.collection("tblProducts").deleteOne({Productid:id},(err,result)=>{
+                if(!err){
+                    console.log("Product Deleted");
                 }
             })
         }
